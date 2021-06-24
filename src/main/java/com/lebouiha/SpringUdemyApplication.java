b345ddfef2483e36077a922051bda6b38a93787a
adnane <the.adnane.1987@gmail.com>
2021-06-24 23:51:38 +0000

    first commit

---
+++ src/main/java/com/lebouiha/SpringUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.lebouiha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUdemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lebouiha/SpringUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lebouiha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
